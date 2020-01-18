6b735f28516c83e2efe8811810aab579e239e2cc
TheZeeT <47123447+TheZeeT@users.noreply.github.com>
2020-01-18 22:40:49 +0200

    Main Menu and some UI

---
+++ Assets/_Scripts/Map/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public Camera Cam;
+    public Light Sun;
+    
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        Cam.gameObject.transform.RotateAround(Vector3.zero, Vector3.up, 10 * Time.deltaTime);
+        Sun.gameObject.transform.Rotate(Vector3.left, 20f * Time.deltaTime);
+        
+
+    }
+
+    public void PlayTheGame()
+    {
+        SceneManager.LoadScene("TestScene");
+    }
+}
